8b4d4a3145e5857b7306d5f194aa25e9ac1b4b91
varpa89 <varpa89@gmail.com>
2021-10-13 18:37:53 +0300

    init commit

---
+++ flutter_deep_linking/lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter Deep Linking'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: const Text('Launch Safari'),
+          onPressed: () async {
+            await launch(
+              'https://swarman.varpa89.ru/authentication_result',
+              forceSafariVC: true,
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
